485e8ebe0d6753327080a3ad1c3a410988afe181
Ömer Faruk Günay <116501544+ofarukgunay@users.noreply.github.com>
2025-07-28 15:53:19 +0300

    update app v2

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import WelcomeScreen from '../WelcomeScreen';
+import React from "react";
+import WelcomeScreen from "../WelcomeScreen";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export default function TabOneScreen() {
-  return <WelcomeScreen />;
+type RootStackParamList = {
+  TabOneScreen: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, "TabOneScreen">;
+
+export default function TabOneScreen({ navigation }: Props) {
+  return <WelcomeScreen navigation={navigation} />;
 }
